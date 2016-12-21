6acb38dea8d47a217cf0bd177c9c9d043d8efbac
Elias Perez <eperez@pinch.me>
2016-12-21 09:43:31 -0500

    Fix name of status field for when order is received.

--- lib/hand_evaluator.rb
+++ lib/hand_evaluator.rb
@@ -1,4 +1,12 @@
+require './lib/hand_ranking'
 class HandEvaluator
   def return_stronger_hand(left, right)
+    hands = [HandRanking.rank(left), HandRanking.rank(right)]
+
+    if hands[0].kind == hands[1].kind
+      hands.max_by(&:points)
+    else
+      hands.max_by(&:kind)
+    end
   end
 end
